d04de13226638f4420cc14b15f12ed25f68b78bf
biradarpooja <89473036+biradarpooja@users.noreply.github.com>
2021-09-07 14:30:52 +0530

    Student Data
    Student Data Project

---
+++ StudentForm.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package studentform;
+
+/**
+ *
+ * @author biradar bharat
+ */
+public class StudentForm {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
